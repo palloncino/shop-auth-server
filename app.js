232ebe632574da94b52a6382cee62b0d3a9c7774
palloncino <antonio.guiotto.dev@gmail.com>
2023-11-23 17:50:16 +0100

    init

--- app.js
+++ app.js
@@ -1,20 +1,30 @@
+require('dotenv').config();
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const express = require("express");
+const cors = require('cors');
 const apiRouter = require('./api');
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
+// Middleware
+app.use(cors()); // Enable CORS for all routes
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-// Routes
+// API routes
 app.use('/api', apiRouter);
 
-app.listen(3000, () => {
-  console.log('listening on port 3000');
-})
+// Error Handling Middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
 
-module.exports = app;
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
